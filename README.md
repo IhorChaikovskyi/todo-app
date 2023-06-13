d434adb0de0a9c35ac597b39631ab4dd9d56987f
Ihor Chaikovskyi <ihorchaikovskyiweb@gmail.com>
2023-06-13 19:35:48 +0300

    add app

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
-# React TypeScript Starter Pack
+# Todo app
 
-To use this template click `Use this template`
+A single-page application that allows users to create and manage a list of tasks.
 
-### Available Scripts
+# [Demo](https://ihorchaikovskyi.github.io/todo-app/);
 
-`Deploy` - available to deploy your application to gh-pages
+# Technologies used
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+* React.js
+* React TRANSITION GROUP
+* React classNames
+* TypeScript
+* JavaScript
+* Fetch, REST API
+* Sass (SCSS)
+* Bulma
+* FontAwesome
+
+# Structure
+
+The application is built through functional components and the use of hooks. Each component can be fully reused.
+
+# Functionality
+
+* All todos are stored and downloaded from the server;
+* User can enter a new todo, which will also be uploaded to the server;
+* User can also modify existing todos;
+* User can highlight all unfinished todos;
+* User can also delete completed todos;
+* There is also a filter which you can show completed or unfinished todos;
+* All changes are saved in the server;
+* In case of server error, user is notified;
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,220 @@
-import React from 'react';
-import './App.scss';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useState, useEffect, useMemo } from 'react';
+import { UserWarning } from './UserWarning';
+import { Todo } from './types/Todo';
+import { TodoFilter } from './types/TodoFilter';
+import {
+  getTodos,
+  deleteTodo,
+  patchTodoCompleted,
+  patchTodoTitle,
+} from './api/todos';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Notification } from './components/Notification';
+import { Errors } from './types/Errors';
+import { Header } from './components/Header';
 
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+export const USER_ID = 9960;
 
 export const App: React.FC = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [selectedFilter, setSelectedFilter] = useState(TodoFilter.ALL);
+  const [typeError, setTypeError] = useState<Errors | null>(null);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [loadingTodoIds, setLoadingTodoIds] = useState<number[]>([]);
+  const [completedAll, setCompletedAll] = useState(false);
+  const [loaderTodo, setLoaderTodo] = useState(0);
+
+  const filteredList = useMemo(() => {
+    if (TodoFilter.ALL !== selectedFilter) {
+      switch (selectedFilter) {
+        case TodoFilter.ACTIVE:
+          return todoList.filter((todo) => !todo.completed);
+        case TodoFilter.COMPLETED:
+          return todoList.filter((todo) => todo.completed);
+        default:
+          break;
+      }
+    }
+
+    return todoList;
+  }, [selectedFilter, todoList]);
+
+  const countItemLeft = todoList?.filter(todo => !todo.completed).length;
+
+  const deleteClickHandlerItem = (id: number) => {
+    deleteTodo(id)
+      .then(() => {
+        const newArray = todoList ? [...todoList] : [];
+
+        setTodoList(newArray.filter(todo => todo.id !== id));
+        setLoaderTodo(0);
+      })
+      .catch(() => {
+        setTypeError(Errors.REMOVE);
+      });
+  };
+
+  const handlerDeleteClickFooter = () => {
+    const completedTodo
+    = todoList?.filter(todo => todo.completed).map(todo => todo.id);
+
+    if (completedTodo) {
+      setLoadingTodoIds(completedTodo);
+    }
+
+    completedTodo?.forEach(todo => {
+      deleteTodo(todo)
+        .then(() => {
+          if (todoList) {
+            setTodoList(
+              todoList.filter(todoFilter => {
+                return !completedTodo.includes(todoFilter.id);
+              }),
+            );
+          }
+        })
+        .catch(() => {
+          setTypeError(Errors.REMOVE);
+        });
+    });
+  };
+
+  const patchHandlerTodoCompleted = (
+    id: number,
+    completed: boolean,
+    bringInList: boolean,
+  ) => {
+    patchTodoCompleted(id, completed)
+      .then(() => {
+        setLoaderTodo(0);
+
+        if (bringInList) {
+          const newArray = todoList?.map(todo => {
+            const newTodo = { ...todo };
+
+            if (todo.id === id) {
+              newTodo.completed = completed;
+
+              return newTodo;
+            }
+
+            return newTodo;
+          });
+
+          if (newArray) {
+            setTodoList(newArray);
+          }
+        }
+      })
+      .catch(() => {
+        setTypeError(Errors.UPDATE);
+      });
+  };
+
+  const patchHandlerTodoTitle = (
+    id: number,
+    title: string,
+  ) => {
+    patchTodoTitle(id, title)
+      .then(() => {
+        setLoaderTodo(0);
+
+        const newArray = todoList?.map(todo => {
+          const newTodo = { ...todo };
+
+          if (todo.id === id) {
+            newTodo.title = title;
+          }
+
+          return newTodo;
+        });
+
+        if (newArray) {
+          setTodoList(newArray);
+        }
+      })
+      .catch(() => {
+        setTypeError(Errors.UPDATE);
+      });
+  };
+
+  const toggleAllHandler = () => {
+    const newArray = todoList.map(todo => {
+      const newTodo = { ...todo };
+
+      if (newTodo.completed === completedAll) {
+        patchHandlerTodoCompleted(newTodo.id, !completedAll, false);
+        newTodo.completed = !completedAll;
+      }
+
+      return newTodo;
+    });
+
+    setCompletedAll(!completedAll);
+
+    if (newArray) {
+      setTodoList(newArray);
+    }
+  };
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(setTodoList)
+      .catch(() => {
+        setTypeError(Errors.UPDATE);
+      });
+  }, []);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          userId={USER_ID}
+          counterItemLeft={countItemLeft}
+          setTypeError={setTypeError}
+          todoList={todoList}
+          setTempTodo={setTempTodo}
+          setTodoList={setTodoList}
+          toggleAllHandler={toggleAllHandler}
+        />
+
+        {todoList && (
+          <TodoList
+            filteringList={filteredList}
+            tempTodo={tempTodo}
+            loadingTodoIds={loadingTodoIds}
+            deleteClickHandler={deleteClickHandlerItem}
+            patchHandlerTodoCompleted={patchHandlerTodoCompleted}
+            patchHandlerTodoTitle={patchHandlerTodoTitle}
+            setLoaderTodo={setLoaderTodo}
+            loaderTodo={loaderTodo}
+          />
+        )}
+
+        {(todoList && todoList?.length !== 0) && (
+          <Footer
+            countItemLeft={countItemLeft}
+            selectedFilter={selectedFilter}
+            setSelectedFilter={setSelectedFilter}
+            todoList={todoList}
+            handlerDeleteClickFooter={handlerDeleteClickFooter}
+          />
+        )}
+
+      </div>
+
+      <Notification
+        setTypeError={setTypeError}
+        typeError={typeError}
+      />
     </div>
   );
 };
